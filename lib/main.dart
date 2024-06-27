f9ea4fca46d92e098eebdba97e01a15a0dabb0ed
abate2283 <alfredbate@yahoo.co.uk>
2024-06-26 21:45:00 -0500

    Created new drivers app

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:userapp/authentication/signup_screen.dart';
+
+Future main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: 'key',
+    appId: 'id',
+    messagingSenderId: 'sendid',
+    projectId: 'flutter-uride-with-admin',
+    storageBucket: 'flutter-uride-with-admin.appspot.com',
+      )
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'URide App',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.blue,
+      ),
+      home: SignupScreen(),
+    );
+  }
+}
+
